4238574bd77b7a760c26dc988612feac5fa55b3d
Serge <sergovendetta@mail.ru>
2019-02-27 20:30:42 +0300

    An android client was created

---
+++ android/app/src/main/java/android/exchanger/exchanger_android_client/MainActivity.java
@@ -0,0 +1,13 @@
+package android.exchanger.exchanger_android_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
